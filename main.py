bee858b45ec1718ce9dada58d0f6f38676481468
Drew Lauck <drewlauck92@gmail.com>
2025-03-14 19:56:25 -0400

    issue with files not deleting via os. added multiprocessing to fix issue

--- main.py
+++ main.py
@@ -6,10 +6,27 @@ import os
 import random
 from gtts import gTTS
 from time import ctime
+from multiprocessing import Process
 
 r = sr.Recognizer()
 
-def record_audio(ask = False):
+def play_sound(file):
+    playsound.playsound(file)
+
+def vito_speak(audio_string):
+    tts = gTTS(text=audio_string, lang='en')
+    audio_file = f'audio-{random.randint(1, 1000000)}.mp3'
+    tts.save(audio_file)
+
+    # Play sound in a separate process
+    p = Process(target=play_sound, args=(audio_file,))
+    p.start()
+    p.join()  # Wait for the sound to finish playing
+
+    # Now remove the file safely
+    os.remove(audio_file)
+
+def record_audio(ask=False):
     with sr.Microphone() as source:
         if ask:
             vito_speak(ask)
@@ -22,16 +39,7 @@ def record_audio(ask = False):
         except sr.RequestError:
             vito_speak("Sorry, my speech service is down.")
         return voice_data
-    
-def vito_speak(audio_string):
-    tts = gTTS(text=audio_string, lang='en')
-    r = random.randint(1, 1000000)
-    audio_file = 'audio-' + str(r) + '.mp3'
-    tts.save(audio_file)
-    playsound.playsound(audio_file)
-    print(audio_string)
-    os.remove(audio_file)
-    
+
 def respond(voice_data):
     if 'what is your name' in voice_data:
         vito_speak("My name is Vito")
@@ -44,14 +52,17 @@ def respond(voice_data):
         vito_speak(f"Here's what I found for {search}")
     if 'find location' in voice_data:
         location = record_audio('What do you want to know the location of?')
-        url = 'https://google.nl/maps/place/' + location + '/&amp;'
+        url = 'https://google.nl/maps/place/' + location
         webbrowser.open(url)
         vito_speak(f"Here's the location of {location}")
     if 'exit' in voice_data:
+        vito_speak("Goodbye!")
         exit()
 
-time.sleep(1)    
-vito_speak("How can I help you?")
-while 1:
-    voice_data = record_audio()
-    respond(voice_data)
+# Windows multiprocessing fix
+if __name__ == "__main__":
+    time.sleep(1)
+    vito_speak("How can I help you?")
+    while True:
+        voice_data = record_audio()
+        respond(voice_data)
